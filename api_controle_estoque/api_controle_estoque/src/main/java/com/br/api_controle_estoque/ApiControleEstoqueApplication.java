6f473f63848d3d67cc5f75220981a4a7c07df48b
Thiago Barros <thiagoo.barros08@gmail.com>
2025-01-29 20:58:08 -0300

    First commit

---
+++ api_controle_estoque/api_controle_estoque/src/main/java/com/br/api_controle_estoque/ApiControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.br.api_controle_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleEstoqueApplication.class, args);
+	}
+
+}
---
+++ api_controle_estoque/api_controle_estoque/src/test/java/com/br/api_controle_estoque/ApiControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.api_controle_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
